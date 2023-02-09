591442a737869179951a76c2a41723448c9d524d
MrZodoxVpython <mrzodoxvpython@gmail.com>
2023-02-09 07:45:10 +0700

    first

---
+++ style.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+    cout << "welcome world";
+    return 0;
+}
---
+++ style.java
@@ -0,0 +1,5 @@
+public class style {
+    public static void style(String, args) {
+        System.out.println("Welcome World");
+    }
+}
