0769fdc44c9a442c9aff7ab688f7d14ae3ec2e41
MrZodoxVpython <wickman@MrZodoxVpython.hackerone.com>
2024-10-03 23:56:11 -0400

    PART 3.1

--- pythonCodeCamp/3.pythonVariableContainer.py
+++ pythonCodeCamp/3.pythonVariableContainer.py
@@ -3,5 +3,6 @@ character_age = "35"
 
 print("There once was a named " + character_name + ", ")
 print("He was " + character_age + " years old ")
+character_name = "Mike"
 print("He really liked the name " + character_name + ", ")
 print("But didn't like being " + character_age + ", ")
