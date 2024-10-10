95b84f87ede5cea5043cb4681f53c8153f63daae
MrZodoxVpython <mrzodoxvpython@gmail.com>
2024-10-11 02:48:01 +0700

    adding loop

---
+++ python/rndm/loopAlpro.py
@@ -0,0 +1,5 @@
+food = ['nasi', 'rawon', 'pecel', 'ayam']
+drink = ['joshua', 'teh', 'coklat', 'milo']
+
+for food, drink in zip(food, drink):
+    print(f"{food} adalah makanan dan {drink} adalah minuman")
