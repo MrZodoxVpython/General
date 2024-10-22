5b84240a91f6c1ed09dfeeedca1bb58ee40b3e7a
MrZodoxVpython <mrzodoxvpython@gmail.com>
2024-10-22 14:14:17 +0700

    adding ITTP

---
+++ javascript/utsITTP.py
@@ -0,0 +1,9 @@
+old = int(input("Type your fucking old: "))
+if old > 50:
+    print("You old")
+if old < 25:
+    print("You a young")
+if old < 17:
+    print("You a adult")
+if old < 7:
+    print("You a shit child")
