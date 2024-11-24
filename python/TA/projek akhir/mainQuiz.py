e933eff7adadc935557e433d166c545944ac6496
MrZodoxVpython <mrzodoxvpython@gmail.com>
2024-11-24 08:58:39 +0700

    adding json for data like question and answer for quiz python program and adjust program to only one file make izi to read and understanable

--- python/TA/projek akhir/mainQuiz.py
+++ python/TA/projek akhir/mainQuiz.py
@@ -1,24 +1,35 @@
 import subprocess
 import threading
-
-def timeout_function():
-    print("Waktu habis!")
-timer = threading.Timer(30.0, timeout_function)
-timer.start()
-answer = input("Jawaban Anda (30 detik untuk menjawab): ")
-timer.cancel()
-print("Jawaban Anda:", answer)
+import json
 
 userData = { 
-    "username1": "password1",  
-    "username2": "password2"
+    "user1": "password1",  
+    "user2": "password2"
 }
-
-def loginPrompt():
+adminData = {
+    "admin1": "password1",
+    "admin2": "password2"
+}
+def whoami():
+    print("Menu")
+    print("1. User Login")
+    print("2. Admin Login")
+    who = input("Masukkan pilihan: ")
+    if who == "1":
+        userPrompt()
+    if who == "2":
+        adminPrompt()
+    else:
+        print("Not Valid!")
+def userPrompt():
     while True:
-        if loginM():
-            menuBerhasil() 
-def loginM():
+        if loginUser():
+            menu() 
+def adminPrompt():
+    while True:
+        if loginAdmin():
+            menuAdmin()
+def loginUser():
     print("\n=== Selamat datang di Quiz International ===") 
     print("=== Silahkan Login dulu ===")
     username = input("Username: ")
@@ -28,7 +39,18 @@ def loginM():
         return True   
     else:
         print("Username atau password salah. Silakan coba lagi.")
-        return False  
+        return False 
+
+def loginAdmin():
+    print("=== Admin Login Form! ===")
+    username = input("Username: ")
+    password = input("Password: ")
+    if username in adminData and adminData[username] == password:
+        print(f"Login berhasil! Welcome, {username}!")
+        return True   
+    else:
+        print("Username atau password salah. Silakan coba lagi.")
+        return False
 
 def tambahPengguna():
     print("\n--- Tambah Pengguna Baru ---")
@@ -41,9 +63,9 @@ def tambahPengguna():
         userData[username] = password
         print(f"Pengguna {username} berhasil ditambahkan!")
 
-def menuBerhasil():
+def menuAdmin():
     while True:
-        print("\n--- Menu Opsional ---")
+        print("\n--- Menu After Login ---")
         print("1. Lihat Profil")
         print("2. Pengaturan Akun")
         print("3. Kembali ke Menu Login")
@@ -58,18 +80,19 @@ def menuBerhasil():
             print("Kembali ke menu utama...")
             break
         elif pilihan == "4":
-            menu_quiz()
+            menu()
         elif pilihan == "5":
             tambahPengguna()
             break
         else:
             print("Pilihan gk valid. Silakan coba lagi.")
 
-def menu_quiz():
-    print("\n--- Masuk ke Menu Quizz ---")
-    menu()
+def load_questions():
+    with open('data.json', 'r') as file:
+        return json.load(file)
 
 def menu():
+    data = load_questions()
     while True:
         print("\nSelamat datang in Quizz")
         print("Pilih Quiz test yang akan anda kerjakan:")
@@ -77,38 +100,46 @@ def menu():
         print("2. Quiz Matematika")
         print("3. Quiz Agama")
         print("4. Quiz Bahasa")
-        print("5. Keluar")  
+        print("5. Exit ke menu after login")
         pilihan = input("Masukkan nomor pilihan: ")
 
         if pilihan == "1":
-            ipa()
+            quiz("IPA", data)
         elif pilihan == "2":
-            matematika()
+            quiz("Matematika", data)
         elif pilihan == "3":
-            agama()
+            quiz("Agama", data)
         elif pilihan == "4":
-            bahasa()
+            quiz("Bahasa", data)
         elif pilihan == "5":
             print("All program are used, thanks!")
             break
         else:
             print("Pilihan gak valid, back to menu")
 
-def ipa():
-    print("Quiz IPA. (Pertanyaan)")
+def quiz(subject, data):
+    print(f"Quiz {subject}.")
+    for item in data[subject]:
+        question = item["question"]
+        answer = item["answer"]
+        user_answer = input(f"{question}\nJawaban: ")
+        if user_answer.lower() == answer.lower():
+            print("Benar!")
+        else:
+            print(f"Salah. Jawaban yang benar adalah: {answer}")
 
-def matematika():
-    print("Matematika. (Pertanyaan)")
 
-def agama():
-    print("Quiz Agama. (Pertanyaan")
+# def menu_quiz():
+#     menu()
+#     print("\n--- batas waktu ---")
+#     menuBerhasil()
 
-def bahasa():
-    print("Quiz Bahasa. (Pertanyaan)")
+# def timeout_function():
+#     print("Waktu habis!")
+# timer = threading.Timer(30.0, timeout_function)
+# timer.start()
+# answer = input("Jawaban Anda (30 detik untuk menjawab): ")
+# timer.cancel()
+# print("Jawaban Anda:", answer)    
 
-def menu_quiz():
-    menu()
-    print("\n--- batas waktu ---")
-    timeout_function()
-    menuBerhasil()
-loginPrompt()
+whoami()
