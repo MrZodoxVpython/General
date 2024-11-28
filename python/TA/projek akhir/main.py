9f0d932944a0af9b82d9f94332e2bae6b40801b2
MrZodoxVpython <mrzodoxvpython@gmail.com>
2024-11-28 08:54:46 +0700

    modified program

--- python/TA/projek akhir/main.py
+++ python/TA/projek akhir/main.py
@@ -1,37 +1,44 @@
 import json
+import os
+import threading
 
 data = {
     "user": {
-        "user1": "password1",
-        "user2": "password2"
+        "user1": "pass1",
     },
     "admin": {
-        "admin1": "password1",
-        "admin2": "password2"
+        "admin1": "pass1",
     }
 }
 
+def logiya():
+    while True: 
+        if loginUser():
+            menuUser()
+        else:
+            os.system('cls')
+            print("Akun yang anda masukkan salah !!")
+
 def loginPrompt(): 
-        print("Menu")
-        print("1. User Login")
-        print("2. Admin Login")
+        print("Login Dashboard")
+        print("1. Login")
+        print("2. SignUp")
         who = input("Masukkan pilihan: ")
         if who == "1":
-            while True:
-                if loginUser():
-                    menuQuiz()
+            logiya()
         if who == "2":
-            while True:
-                if loginAdmin():
-                    menuAdmin()
+            userSignUp()
         else:
             print("=!= Masukkan (1/2) =!=")
     
 def loginUser():
-    print("\n=== Selamat datang di Quiz International ===") 
-    print("=== Silahkan Login dulu ===")
+    print("\n=== WELCOME TO CLASH OF CHAMPIONS ===") 
+    print("=== Silahkan Login terlebih dahulu ===")
     username = input("Username: ")
     password = input("Password: ")
+    if username == "$admin" and password == "$admin":
+        if loginAdmin():
+            menuAdmin()
     if username in data["user"] and data["user"][username] == password:
         print(f"Login berhasil! Selamat datang, {username}")
         return True   
@@ -44,17 +51,13 @@ def loginAdmin():
     username = input("Username: ")
     password = input("Password: ")
     if username in data["admin"] and data["admin"][username] == password:
-        print(f"Login berhasil! Welcome, {username}!")
+        print(f"\nLogin berhasil! Welcome, {username}!")
         return True   
     else:
         print("Username atau password salah. Silakan coba lagi.")
         return False
 
-def load_questions():
-    with open('data.json', 'r') as file:
-        return json.load(file)
-
-def menuQuiz():
+def menuUser():
     data = load_questions()
     while True:
         print("\nSelamat datang in Quizz")
@@ -78,24 +81,53 @@ def menuQuiz():
             break
         else:
             print("Pilihan gak valid, back to menu")
+       
+def load_questions():
+    with open('data.json', 'r') as file:
+        return json.load(file)        
+
+def timeout_function():
+    print("Waktu habis!")  
 
 def quiz(subject, data):
     print(f"Quiz {subject}.")
     for item in data[subject]:
         question = item["question"]
         answer = item["answer"]
-        user_answer = input(f"{question}\nJawaban: ")
+
+        timer = threading.Timer(15.0, timeout_function)
+        timer.start()  
+        user_answer = input(f"{question}, 15 detik untuk menjawab!\nJawaban: ")
+        timer.cancel()
         if user_answer.lower() == answer.lower():
             print("Benar!")
         else:
             print(f"Salah. Jawaban yang benar adalah: {answer}")
 
+def pengaturanQuiz():
+    print("1. Lihat daftar quiz")
+    print("2. Daftar quiz")
+    print("3. Update quiz")
+    print("4. Hapus quiz")
+    pQ = input("Masukkan pilihan:") 
+    if pQ == "1":
+        data = load_questions()
+        for kategori, daftar_qa in data.items():
+            print(f"Kategori: {kategori}")
+            print("-" * 50)
+    
+            for item in daftar_qa:
+                print(f"Pertanyaan: {item['question']}")
+                print(f"Jawaban: {item['answer']}")
+                print("-" * 20)  
+                print("=" * 50)
+  
 def menuAdmin():
     while True:
         print("\n=== Admin Menu ===")
         print("1. Create New User")
         print("2. Read Credential")
-        print("3. Masuk Ke Menu Quizz")
+        print("3. Pengaturan Quizz")
         print("4. Kembali ke menu login")
         pilihan = input("Masukkan menu: ")
         if pilihan == "1":
@@ -103,20 +135,32 @@ def menuAdmin():
         elif pilihan == "2":
             lihatPengguna()
         elif pilihan == "3":
-            menuQuiz()
+            pengaturanQuiz()
         elif pilihan == "4":
             break
         else:
             print("Pilihan gk valid. Silakan coba lagi.")
+
+def userSignUp():
+        user = input("Masukkan username: ")
+        if user in data:
+            print("Username sudah ada. Silakan coba username lain.")
+        else:
+                password = input("Masukkan password: ")
+                data["user"][user] = password
+                print(f"Akun {user} berhasil di daftarkan!")
+                loginPrompt()
+
 def lihatPengguna():
         print("\nUser credential:")
         user_data = data["user"].items()
         for username, password in user_data:
-            print(f"Username: {username}, Password: {password}") 
-            print("\nAdmin credential:")
+            print(f"Username: {username}\nPassword: {password}") 
+        print("\nAdmin credential:")
         admin_data = data["admin"].items()
         for username, password in admin_data:
-            print(f"Username: {username}, Password: {password}")
+            print(f"Username: {username}\nPassword: {password}")
+
 def tambahPengguna():
         print("\n--- Tambah Pengguna Baru ---")
         print("1.Tambah pengguna user")
@@ -144,13 +188,5 @@ def tambahPengguna():
         except ValueError:
             print("\nHanya boleh berupa angka 1/2")
             tambahPengguna()
-            
-# def timeout_function():
-#     print("Waktu habis!")
-# timer = threading.Timer(30.0, timeout_function)
-# timer.start()
-# answer = input("Jawaban Anda (30 detik untuk menjawab): ")
-# timer.cancel()
-# print("Jawaban Anda:", answer)    
 
-loginPrompt()
+pengaturanQuiz()
